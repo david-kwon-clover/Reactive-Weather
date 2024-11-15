5490abb2746783f64a515731fc59c2f608c3e424
David Kwon <david.kwon@clover.com>
2024-11-15 08:26:48 -0700

    Create Form component and import in App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import cities from "./data";
 import WeatherCard from './components/WeatherCard';
 import Location from './components/Location';
+import Form from './components/Form';
 
 function App() {
     const [location, setLocation] = useState("New York City");
@@ -10,6 +11,7 @@ function App() {
         <>
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
+            <Form location={location} setLocation={setLocation} />
             <Location data={cities} location={location} setLocation={setLocation} />
             <div className = "app">
                 {cities.map((city, index) => {
