291355b5e2742557b4bf97bbd9fc90e9713da1da
David Kwon <david.kwon@clover.com>
2024-11-15 08:48:15 -0700

    Add functionality to Form component

--- components/Form.js
+++ components/Form.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = ({ location, setLocation }) => {
+  const [typedLocation, setTypedLocation] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setLocation(typedLocation);
+    setTypedLocation("");
+  }
+
   return (
-    <div>Form</div>
-  )
-}
+    <form className="form" onSubmit={handleSubmit}>
+      <label className="city">
+        City:
+        <input type="text" placeholder="type your city here..." value={typedLocation} onChange={(e) => setTypedLocation(e.target.value)}/>
+      </label>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
